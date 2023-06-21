be40963bf72a83050324ccbf8fa60615b32fbfc5
Sumit888d <77741088+Sumit888d@users.noreply.github.com>
2023-06-21 20:31:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,6 @@ for(let key of keys){
             ){
                 input+=")";
             }
-
             display_input.innerHTML=CleanInput(input);
         } else{
             if(validateInput(value)){
@@ -49,7 +48,6 @@ for(let key of keys){
 function CleanInput(input){
     let input_array=input.split("");
     let input_array_length=input_array.length;
-
     for(let i=0;i<input_array_length;i++){
         if(input_array[i]=="*"){
          input_array[i]=`<span class="operator">x</span> `   ;
@@ -78,11 +76,10 @@ function CleanInput(input){
 
 function CleanOutput(output){
     let output_string=output.toString();
+    output_string=Number(output_string).toPrecision(5);
     let decimal=output_string.split(".")[1];
     output_string=output_string.split(".")[0];
-
     let output_array =  output_string.split("");
-
     if(output_array.length>3){
         if(output=="Infinity"){
             return "Infinity";
@@ -91,7 +88,6 @@ function CleanOutput(output){
           output_array.splice(i,0,",");
         }
     }
-
     if(decimal){
         output_array.push(".");
         output_array.push(decimal);
@@ -102,11 +98,9 @@ function CleanOutput(output){
 function validateInput(value){
     let last_input=input.slice(-1);
     let operators=["+","-","*","/"];
-
     if(value =="." && last_input=="."){
         return false;
     }
-
     if(operators.includes(value)){
         if(operators.includes (last_input)){
             return false;
@@ -120,7 +114,6 @@ function validateInput(value){
 
 function PrepareInput(input){
 let input_array=input.split("");
-
 for(let i=0;i<input_array.length;i++){ 
     if(input_array[i]=="%"){
         input_array[i]="/100";
